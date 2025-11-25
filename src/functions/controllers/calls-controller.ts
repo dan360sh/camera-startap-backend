3cf9053a7de2884a8b4f722c6be4d452af757cdd
Dan360sh <swqazxcd@gmail.com>
2025-11-26 00:01:22 +0300

    создание звонка

--- src/functions/controllers/calls-controller.ts
+++ src/functions/controllers/calls-controller.ts
@@ -1,3 +1,4 @@
+import { Observable } from "rxjs";
 import { OnMessage, WebSocketService } from "../services/webSocket";
 
 export class CallsController { 
@@ -6,42 +7,45 @@ export class CallsController {
     }
     
     init() {
-        // this.webSocketService.onMessage.subscribe((e: OnMessage) => {
-        //     const data = e.data;
-        //     const ws = e.ws;
-        //         if(data.type === 'newName') {
-        //             console.log('newName', data);
-        //             this.webSocketService.listUsers.push({name: data.message, ws: ws, status: 'online'});
-        //             //nameUser = data.message;
-        //             for(let user of this.webSocketService.listUsers) {
-        //                 user.ws.send(JSON.stringify({type: 'user_list', message: this.webSocketService.listUsers.map( (e) => {return {name: e.name, status: e.status}})}));
-        //             }
-        //         }
-        //         //исходящий вызов
-        //         if(data.type === 'outgoing_call') {
-        //             console.log('call',  data.message);
-        //             const find = this.webSocketService.listUsers.find(e=> e.name === data.message.user);
-        //             if(find) {
-        //                 console.log('call find', find.name);
-        //                 find.ws.send(JSON.stringify({type: 'outgoing_call', message: {offer: data.message.offer}}));
-        //             }
-        //         }
-        //         //входящий
-        //         if(data.type === 'incoming_call') {
-        //           const find = this.webSocketService.listUsers.find(e=> e.name === data.message.user);
-        //           if(find) {
-        //                 find.ws.send(JSON.stringify({type: 'incoming_call', message: {offer: data.message.offer}}));
-        //           }
-        //         }
+        this.webSocketService.onMessageAjax('call', async (e: OnMessage, brake: Observable<boolean>) => {
+            console.log("Привет мир",e. data);
+            const findUser = this.webSocketService.listUsers.find(el => el.id == e.data.id);
+            if (findUser) {
+                const r: any = await findUser.ws.sendAjax('incoming-call', {id: e.ws.codeWs});
+                console.log("ответ", r);
+                if (r) {
+                    return {id: findUser.ws.codeWs}
+                }
+            } 
+            return {
+                errorCode: "stop"
+            }
+        });
 
-        //         //icecandidate
-        //         if(data.type === 'icecandidate') {
-        //           const find = this.webSocketService.listUsers.find(e=> e.name === data.message.user);
-        //           if(find) {
-        //             find.ws.send(JSON.stringify({type: 'icecandidate', message: {candidate: data.message.candidate}}));
-        //           }
-        //         }
-        // })
+        this.webSocketService.onMessage('icecandidate', (e) => {
+            const find = this.webSocketService.listUsers.find(el => el.ws.codeWs == e.data.id);
+            if(find) {
+                find.ws.send('icecandidate', e.data.candidate);
+            }
+            
+        });
+
+        this.webSocketService.onMessageAjax('offer',  async (e: OnMessage, brake: Observable<boolean>) => {
+          console.log('offer e', e)
+          const findUser = this.webSocketService.listUsers.find(el => el.id == e.data.id);
+          console.log('offer findUser', findUser)
+            if (findUser) {
+                 const r: any = await findUser.ws.sendAjax('incoming-offer', {id: e.ws.codeWs, offer: e.data.offer});
+                 console.log('offer r', r)
+                if (r) {
+                    return {id: findUser.ws.codeWs,  offer: e.data.offer}
+                }
+            }
+            return {
+                errorCode: "stop"
+            }
+            
+        });
         
     }
 }
--- src/functions/services/webSocket.ts
+++ src/functions/services/webSocket.ts
@@ -1,5 +1,5 @@
 import { WebSocketServer } from "ws";
-import { Observable, Subject } from 'rxjs';
+import { firstValueFrom, Observable, Subject } from 'rxjs';
 import crypto  from 'crypto';
 export interface UsersStatus {
     name: string,
@@ -10,21 +10,56 @@ export interface UsersStatus {
 }
 export interface OnMessage  {ws: Ws, data: any};
 export class Ws {
-    constructor(public readonly ws: any, public readonly codeWs: string) {}
-    send(data: any) {
+    constructor(public readonly ws: any, public readonly codeWs: string, public readonly listUser: UsersStatus[], public readonly responseAjax: Subject<any>) {}
+    code: string | null = null;
+    send(type: string, data: any) {
         console.log(JSON.stringify(data), "data123")
-        this.ws.send(JSON.stringify(data));
+        this.ws.send(JSON.stringify({type: type, message: data}));
     }
 
-    userSend(data: any) {
-        data = JSON.stringify(data);
+    async sendAjax(type: string, message: any, timeConnect: number = 100000) {
+        
+        this.code = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+            const r = Math.random() * 16 | 0;
+            const v = c == 'x' ? r : (r & 0x3 | 0x8);
+            return v.toString(16);
+        });
+        this.ws.send(JSON.stringify({message, type, code: this.code, timeConnect, to: 'front'}));
+        
+        const p =  new Promise((resolve) => {
+            this.responseAjax.subscribe((e) => {
+            if (e.code == this.code) {
+                if(e.errorCode == 'stop') {
+                resolve({errorCode: 'stop'});
+                } else {
+                resolve(e.message);
+                }
+            }
+            })
+        });
+        return p;
+        
     }
+
+    stop () {
+        this.responseAjax.next({code: this.code, errorCode: 'stop'});
+    }
+
+    // userSend(data: any, id: string) {
+    //     data = JSON.stringify(data);
+    //     const userFilter = this.listUser.filter(e => e.id == id);
+    //     for(let user of userFilter) {
+    //         user.ws.send(data);
+    //     }
+    // }
 }
 export class WebSocketService {
 
     private wss! : WebSocketServer;
     listUsers: UsersStatus[] = [];
     private readonly _onMessage = new Subject<OnMessage>();
+    // Ответ от фронта
+    responseAjax = new Subject();
     //для обычных запросов
     onMessage!: (type: string, fun: (e: OnMessage) => void) => void;
     //Запрос с ответом 
@@ -65,7 +100,7 @@ export class WebSocketService {
                         try {
                             const message = await fun(e, brake.asObservable());
                             if(b) {
-                                e.ws.send({type, message, code, to: 'server'});
+                                e.ws.ws.send(JSON.stringify({type, message, code, to: 'server'}));
                             }
                         
                         }catch (err){
@@ -93,7 +128,14 @@ export class WebSocketService {
             
              console.log("message", message+ "");
              try {
-                this._onMessage.next({ws: new Ws(ws, codeWs), data: JSON.parse(message)});
+                const data = JSON.parse(message);
+                if(data.to == 'front') {
+                    this.responseAjax.next(data);
+                    
+                } else {
+                    this._onMessage.next({ws: new Ws(ws, codeWs, this.listUsers, this.responseAjax), data: data});
+                }
+                
 
              } catch (e) {
                 console.log("ошибка",e)
@@ -124,9 +166,9 @@ export class WebSocketService {
                 }
             });
             console.log(listFilter, "listFilter");
-            user.ws.send({type: 'user_list', message: listFilter.map(e => { return {
+            user.ws.send('user_list', listFilter.map(e => { return {
                     name: e.name, status: e.status, id: e.id, location: e.location
-            }})});  
+            }}));  
         }
     }
 }
